80854799fd0c7d49fb6a80143da7e347f120b833
VidyalaxmiTumkur <40994784+VidyalaxmiTumkur@users.noreply.github.com>
2023-07-13 11:44:31 -0400

    fetching a random quote from API array

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let apiQuotes = [];
 
+//Showing New Quote 
+function newQuote() {
+    //Picking a random quote from apiQuote array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
 // Get quotes from API 
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes[1]);
+        newQuote();
     } catch(error){
         //Handle catch error
     }
