0125618d56ba8329855fef1b4ec57e7590cfe402
VidyalaxmiTumkur <40994784+VidyalaxmiTumkur@users.noreply.github.com>
2023-07-13 15:51:41 -0400

    the page now shows random data for quote text and author when the page is refereshed

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+const quoteContainer = document.getElementById('container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 //Showing New Quote 
 function newQuote() {
     //Picking a random quote from apiQuote array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
+
+//for fetching the data locally 
+// function newQuote() {
+//     //Picking a random quote from apiQuote array
+//     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//     console.log(quote);
+// }
 
 // Get quotes from API 
 async function getQuotes() {
