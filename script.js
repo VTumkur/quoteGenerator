cbcb6099d181e13da6830135948b3a39800d5980
VidyalaxmiTumkur <40994784+VidyalaxmiTumkur@users.noreply.github.com>
2023-07-13 10:55:42 -0400

    fetching data from API - returning single data currently

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Testing!')
+let apiQuotes = [];
+
+// Get quotes from API 
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes[1]);
+    } catch(error){
+        //Handle catch error
+    }
+}
+
+//On Load
+getQuotes();
